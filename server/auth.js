70f94e8292fb35e0dfdd9e5a8ab1bfd978e948d4
Noah <noah@noahveltman.com>
2017-01-04 15:57:42 -0800

    skipUser to prevent populating req.user

--- server/auth.js
+++ server/auth.js
@@ -5,7 +5,9 @@ module.exports = function(authFile){
   var basic = auth.basic({
       file:   authFile,
       realm:  "Audiogram Administration",
-      msg401: "Error: Your account details could not be authenticated."
+      msg401: "Error: Your account details could not be authenticated.",
+      msg407: "Error: Your account details could not be authenticated.",
+      skipUser: true
   });
 
   return auth.connect(basic);
