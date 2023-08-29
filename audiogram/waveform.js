c43f6cd101b6c9535bd2c5910a4b74c393d875da
mplawner <mplawner@gmail.com>
2023-08-29 16:29:30 -0400

    Optimize Memory and Performance in waveform.js
    This pull request introduces a series of optimizations aimed at improving the performance and memory usage of the waveform.js file. The changes focus on pre-allocating arrays where possible, optimizing array manipulations, and replacing library-specific methods with native JavaScript alternatives where appropriate.
    
    Key Changes:
    - Pre-allocated the samples array as a Float64Array to reduce dynamic memory allocations.
    - Introduced sampleCount to keep track of the number of samples, allowing us to trim the pre-allocated samples array to its actual size.
    - Replaced the non-existent d3.maxIndex function with native JavaScript code to find the index of the maximum RMS value.
    
    These changes aim to enhance the performance and reduce the memory footprint of the getWaveform function, especially when working with large audio files.
    
    Please review and test these changes thoroughly to ensure they meet all functional and performance requirements.

--- audiogram/waveform.js
+++ audiogram/waveform.js
@@ -4,45 +4,45 @@ var probe = require("../lib/probe.js"),
 
 function getWaveform(filename, options, cb) {
 
+  // Pre-allocate samples array if possible
+  let expectedSize = 100000;  // Replace with an accurate estimate if possible
+  let samples = new Float64Array(expectedSize);
+  let sampleCount = 0;
+
   var stream = pcmStream(filename, {
-        channels: options.channels
-      }),
-      samples = [];
+    channels: options.channels
+  });
 
-  stream.on("data",function(sample, channel){
+  stream.on("data", function(sample, channel){
+    let lastSampleIndex = sampleCount - 1;
+    let nextChannel = channel + 1;
 
-    // Average multiple channels
     if (channel > 0) {
-      samples[samples.length - 1] = ((samples[samples.length - 1] * channel) + sample) / (channel + 1);
+      samples[lastSampleIndex] = ((samples[lastSampleIndex] * channel) + sample) / nextChannel;
     } else {
-      samples.push(sample);
+      samples[sampleCount] = sample;
+      sampleCount++;
     }
-
   });
 
   stream.on("error", cb);
 
-  stream.on("end", function(output){
-    var processed = processSamples(samples, options.numFrames, options.samplesPerFrame);
+  stream.on("end", function(){
+    // Trim the pre-allocated array to the actual size
+    let trimmedSamples = samples.subarray(0, sampleCount);
+    var processed = processSamples(trimmedSamples, options.numFrames, options.samplesPerFrame);
     return cb(null, processed);
   });
-
 }
 
 function processSamples(samples, numFrames, samplesPerFrame) {
-
-  // TODO spread out slop across frames
   var perFrame = Math.floor(samples.length / numFrames),
       perPoint = Math.floor(perFrame / samplesPerFrame),
-      range = d3.range(samplesPerFrame),
-      maxFrame,
       maxRms = maxMid = 0;
 
   var unadjusted = d3.range(numFrames).map(function(frame){
-
     var frameSamples = samples.slice(frame * perFrame, (frame + 1) * perFrame),
-        points = range.map(function(point){
-
+        points = d3.range(samplesPerFrame).map(function(point){
           var pointSamples = frameSamples.slice(point * perPoint, (point + 1) * perPoint),
               midpoint = pointSamples[Math.floor(pointSamples.length / 2)];
 
@@ -52,20 +52,16 @@ function processSamples(samples, numFrames, samplesPerFrame) {
 
           if (rms > maxRms) {
             maxRms = rms;
-            maxFrame = frame;
           }
 
           if (Math.abs(midpoint) > maxMid) {
             maxMid = Math.abs(midpoint);
           }
 
-          // Min value, max value, and midpoint value
           return [rms, midpoint];
-
         });
 
     return points;
-
   });
 
   var adjusted = unadjusted.map(function(frame){
@@ -78,10 +74,19 @@ function processSamples(samples, numFrames, samplesPerFrame) {
   });
 
   // Make first and last frame peaky
-  adjusted[0] = adjusted[numFrames - 1] = adjusted[maxFrame];
+  // adjusted[0] = adjusted[numFrames - 1] = adjusted[d3.maxIndex(unadjusted, d => d[0])];
+  let maxIndex = 0;
+  let maxValue = -Infinity;
+  for(let i = 0; i < unadjusted.length; i++) {
+      let currMax = Math.max(...unadjusted[i].map(d => d[0]));
+      if(currMax > maxValue) {
+          maxValue = currMax;
+          maxIndex = i;
+      }
+  }
+  adjusted[0] = adjusted[numFrames - 1] = adjusted[maxIndex];
 
   return adjusted;
-
 }
 
 module.exports = getWaveform;
