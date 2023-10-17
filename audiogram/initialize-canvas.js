244aaae7bb5efd2ec8cedcbf6bb7fe1f158fa12c
Binh Ly <binh@binh.ly>
2023-10-17 01:42:39 -0400

    Read remote files

--- audiogram/initialize-canvas.js
+++ audiogram/initialize-canvas.js
@@ -1,7 +1,12 @@
 var fs = require("fs"),
     path = require("path"),
     Canvas = require("canvas"),
+    logger = require("../lib/logger/"),
+
     getRenderer = require("../renderer/");
+    
+
+    const download = require('image-downloader');
 
 function initializeCanvas(theme, cb) {
 
@@ -12,6 +17,25 @@ function initializeCanvas(theme, cb) {
     return cb(null, renderer);
   }
 
+  const options = {
+    url: theme.imageURL,
+    dest: '/tmp/',               // will be saved to /path/to/dest/image.jpg
+  };
+  
+  download.image(options)
+    .then(({ filename }) => {
+      logger.debug('Saved to', filename); // saved to /path/to/dest/image.jpg
+
+      var bg = new Canvas.Image;
+      bg.src = filename; //raw;
+      renderer.backgroundImage(bg);
+
+      return cb(null, renderer);
+
+    })
+    .catch((err) => console.error(err));
+
+    /*
   // Load background image from file (done separately so renderer code can work in browser too)
   fs.readFile(path.join(__dirname, "..", "settings", "backgrounds", theme.backgroundImage), function(err, raw){
 
@@ -25,7 +49,7 @@ function initializeCanvas(theme, cb) {
 
     return cb(null, renderer);
 
-  });
+  }); */
 
 }
 
